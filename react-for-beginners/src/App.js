6050521e1180ffaa8c7e873fd33257f784b72db7
jeeweb <52396673+jeeweb@users.noreply.github.com>
2022-07-22 13:59:16 +0900

    useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,31 @@
-import Button from "./Button";
-import styles from "./App.module.css"
+import {useState, useEffect} from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue(prev => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("I run only once.")
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.")
+    if(keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword)
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.")
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword & counter change")
+  }, [keyword, counter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"}/>
+      <input type="text" value={keyword} onChange={onChange} placeholder="Search here"></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
